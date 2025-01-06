ad64a740bb18b504320f709bd5f15782b47a3033
Diego Marques <fariia_di@hotmail.com>
2025-01-06 11:10:36 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/diegomarques/textrix/TextrixApplication.java
@@ -0,0 +1,13 @@
+package br.com.diegomarques.textrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextrixApplication.class, args);
+	}
+
+}
